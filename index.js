6c4dc57d90a7b8015227e83f782bf0d63e312373
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2021-09-30 22:50:49 +0900

    Revert "debug body"
    This reverts commit c31d010801aab1d180a34903e6bc875aded9f660.

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ app
   .post('/webhook', multerUpload.none(), async (req, res) => {
     res.sendStatus(200);
     const form = req.body;
-    console.log(form);
     const mailTo = form.to.match(emailRegexp);
     if (!mailTo) {
       debug('Invalid To address.');
